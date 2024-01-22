5d41c33a5301568bb30340e3180c9309c59fc747
Purvi Chaurasia <chaurasiapurvi@gmail.com>
2024-01-22 19:22:28 +0530

    Migrated database

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAQ2PAdBQayKlalSnJDq30RVTY4Up5PtOo",
-    authDomain: "flowchart-qc.firebaseapp.com",
-    databaseURL: "https://flowchart-qc-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "flowchart-qc",
-    storageBucket: "flowchart-qc.appspot.com",
-    messagingSenderId: "485452533948",
-    appId: "1:485452533948:web:34c4b2785f63e3b97700b9"
+  apiKey: "AIzaSyCbX-x7wlTvfwdU1l5IbQRKhOqo3b8Rtn8",
+  authDomain: "flowchart-qa-verification.firebaseapp.com",
+  databaseURL: "https://flowchart-qa-verification-default-rtdb.firebaseio.com",
+  projectId: "flowchart-qa-verification",
+  storageBucket: "flowchart-qa-verification.appspot.com",
+  messagingSenderId: "937000389632",
+  appId: "1:937000389632:web:1cedfb2b969ea57f405ce5",
+  measurementId: "G-Z9YV9B7WPJ"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
